79ab7f456a63299aefa2bfc60605ee99401cd1d3
HARSH-SHETH <ycdtharsh@gmail.com>
2020-12-31 13:01:00 +0530

    updated dot files

--- update.sh
+++ update.sh
@@ -2,7 +2,8 @@
 cat ~/.config/nvim/init.vim > ./myneovimrc
 cat ~/.vimrc > ./myvimrc
 cat ~/.config/i3/config > i3config
+cat ~/.zshrc > zshrc
 echo "updated dot files"
-git add myneovimrc myvimrc i3config
+git add .
 git commit -m "updated dot files"
 git push origin master
